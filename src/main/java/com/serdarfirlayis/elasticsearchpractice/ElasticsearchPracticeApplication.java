8f1475ab860afa5e4edf410f929dfc203a9f71b0
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2023-09-01 12:21:54 +0300

    first commit

---
+++ src/main/java/com/serdarfirlayis/elasticsearchpractice/ElasticsearchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.serdarfirlayis.elasticsearchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serdarfirlayis/elasticsearchpractice/ElasticsearchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serdarfirlayis.elasticsearchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
