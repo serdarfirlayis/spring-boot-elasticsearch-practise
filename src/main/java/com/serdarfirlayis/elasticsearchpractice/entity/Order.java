88a83571a7a2b38f2a3c0b4b64f77d255e58bbd1
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2023-09-21 02:39:59 +0300

    feat: Entity and model classes were updated

--- src/main/java/com/serdarfirlayis/elasticsearchpractice/entity/Order.java
+++ src/main/java/com/serdarfirlayis/elasticsearchpractice/entity/Order.java
@@ -24,4 +24,40 @@ public class Order {
 
     @Field(type = FieldType.Text)
     private List<String> category;
+
+    @Field(type = FieldType.Keyword)
+    private String currency;
+
+    @Field(type = FieldType.Keyword, name = "customer_id")
+    private String customerId;
+
+    @Field(type = FieldType.Text, name = "customer_first_name")
+    private String customerFirstName;
+
+    @Field(type = FieldType.Text, name = "customer_last_name")
+    private String customerLastName;
+
+    @Field(type = FieldType.Text, name = "customer_full_name")
+    private String customerFullName;
+
+    @Field(type = FieldType.Keyword, name = "customer_gender")
+    private String customerGender;
+
+    @Field(type = FieldType.Keyword, name = "customer_phone")
+    private String customerPhone;
+
+    @Field(type = FieldType.Keyword, name = "email")
+    private String customerEmail;
+
+    @Field(type = FieldType.Keyword, name = "day_of_week")
+    private String dayOfWeek;
+
+    @Field(type = FieldType.Integer, name = "day_of_week_i")
+    private Integer dayOfWeekI;
+
+    @Field(type = FieldType.Text, name = "manufacturer")
+    private List<String> manufacturer;
+
+    @Field(type = FieldType.Date, name = "order_date")
+    private String orderDate;
 }
--- src/main/java/com/serdarfirlayis/elasticsearchpractice/model/OrderResponse.java
+++ src/main/java/com/serdarfirlayis/elasticsearchpractice/model/OrderResponse.java
@@ -15,4 +15,16 @@ public class OrderResponse {
 
     private String id;
     private List<String> category;
+    private String currency;
+    private String customerId;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerFullName;
+    private String customerGender;
+    private String customerPhone;
+    private String customerEmail;
+    private String dayOfWeek;
+    private Integer dayOfWeekI;
+    private List<String> manufacturer;
+    private String orderDate;
 }
